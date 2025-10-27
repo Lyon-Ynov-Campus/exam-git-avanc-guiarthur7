a13e0df597f7c98b05062d219944ef1080572dcc
guiarthur <arthur.guillaume@ynov.com>
2025-10-27 15:16:31 +0100

    rebase color sur dev msg

--- main.go
+++ main.go
@@ -6,8 +6,8 @@ import (
 	"net/http"
 )
 
-func URLHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "<h1>URL visitée</h1><p>Vous avez visité : %s</p>", r.URL.Path)
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
 }
 
 func main() {
@@ -15,7 +15,7 @@ func main() {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
 	})
 
-	http.HandleFunc("/url", URLHandler)
+	http.HandleFunc("/color", ColorHandler)
 
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))

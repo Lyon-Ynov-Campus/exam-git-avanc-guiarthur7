e20b8060901b01a069a6a3d9898de53f843e9aca
guiarthur <arthur.guillaume@ynov.com>
2025-10-27 15:38:28 +0100

    colorfeat

--- main.go
+++ main.go
@@ -10,10 +10,16 @@ func URLHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "<h1>URL visitée</h1><p>Vous avez visité : %s</p>", r.URL.Path)
 }
 
+func ColorHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "<h1>Colors</h1><style>*{background-color: #006400;}</style>")
+}
+
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
 	})
+
+	http.HandleFunc("/color", ColorHandler)
 
 	http.HandleFunc("/url", URLHandler)
 
